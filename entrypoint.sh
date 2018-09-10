d9155c87f25354fc7e75e230a7c3020fdeb0b31d
tonytan4ever <tonytan198211@gmail.com>
2018-09-09 20:52:09 -0500

    Update docker files and redirect output files

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Start running security test suite..."
+
+/usr/bin/syntribos --config-file ${config_file_location} run > /mount-opt/${MY_JOB_NAME}_$(date +%d-%b-%H_%M)_result.txt 2>&1
+
+echo "End run."
