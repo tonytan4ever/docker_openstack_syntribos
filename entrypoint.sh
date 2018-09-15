1304fc4ce8573fb0fe1eec74da226b6eb87623c5
tonytan4ever <tonytan198211@gmail.com>
2018-09-15 16:33:22 -0400

    Add syntribos run folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Start running security test suite..."
 
-/usr/bin/syntribos --config-file ${config_file_location} run > /mount-opt/${MY_JOB_NAME}_$(date +%d-%b-%H_%M)_result.txt 2>&1
+/usr/bin/syntribos --config-file ${config_file_location} run > /mount-opt/syntribos_run/${MY_JOB_NAME}_$(date +%d-%b-%H_%M)_result.txt 2>&1
 
 echo "End run."
