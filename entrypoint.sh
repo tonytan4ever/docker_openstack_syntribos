b50619f8a5e74d88917a26be0c925fcdb73ff16a
tonytan4ever <tonytan198211@gmail.com>
2018-10-03 10:31:43 -0400

    Update logs and colorize etc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Start running security test suite..."
 
-/usr/bin/syntribos --config-file ${config_file_location} run > /mount-opt/syntribos_run/${MY_JOB_NAME}_$(date +%d-%b-%H_%M)_result.txt 2>&1
+/usr/bin/syntribos --config-file ${config_file_location} --no_colorize run > /mount-opt/syntribos_run/${MY_JOB_NAME}_$(date +%d-%b-%H_%M)_result.txt 2>&1
 
 echo "End run."
